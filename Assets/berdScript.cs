487e9a6c522621076b60112f3b8a24b0fd857f8a
BINCY BENNY <bincybenny2608@gmail.com>
2025-11-22 18:55:34 +0530

    Added GameOver and Score

--- Assets/berdScript.cs
+++ Assets/berdScript.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class berdScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float flapStrength;
     public Rigidbody2D myRigidbody;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)==true) 
+        if(Input.GetKeyDown(KeyCode.Space) && birdIsAlive) 
         {
-            myRigidbody.velocity = Vector2.up *10;
+            myRigidbody.velocity = Vector2.up *flapStrength;
         }
+        if(transform.position.y > Camera.main.orthographicSize || transform.position.y < -Camera.main.orthographicSize)
+        {
+            logic.GameOver();
+            birdIsAlive = false;
+        }
+
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        logic.GameOver();
+        birdIsAlive = false;
+    }
+
+
 }
